be2a909ac4f73d86b2683faabb51ebadbe10d014
Nick Tallant <ndtallant@UCHICAGO.EDU>
2018-10-17 21:24:06 -0500

    Quick Summary

--- explore.R
+++ explore.R
@@ -5,20 +5,22 @@
 
 quick.summary <- function(df){
   # Shows each column, if it has nans, its type, and an example value
-  rv <- Data.Frame() 
-  cols <- c("Feature"   # column
-          , "% Missing" # sum(is.null(column)) / length(column)
-          , "Type"      # class(column)
-          , "Uniques"   # length(unique(column))
-          , "Example"   # column[1]
-           ) 
-  
-}
-
-percent.missing <- function(column){
-  return(sum(is.null(column)) / length(column))
-}
-
-nunique <- function(column){
-  return(length(unique(column)))
+  N <- length(df)
+  rv <- data.frame(Feature = colnames(df)
+                 , Missing = rep(NA, N)
+                 , Type = rep("", N)
+                 , Uniques = rep(NA, N)
+                 , Example = rep(NA, N)
+                 , stringsAsFactors = FALSE)
+  i <- 1
+  for (feature in rv$Feature){
+    column <- df[, feature]
+    rv[i, ] <- list(feature
+                  , paste(sum(is.null(column)) / length(column), "%")
+                  , class(column)
+                  , length(unique(column))
+                  , column[1])
+    i <- i + 1
+  }
+  return(rv)
 }
